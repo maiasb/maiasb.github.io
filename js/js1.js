fb88676ee260eda825c9d5d313f0bc8b1526ed39
Caio Maia <caio.maiasb@gmail.com>
2021-05-18 14:45:44 -0300

    Função JavaScript abre e fecha sidebar; Estilização dos elementos do navbar

--- js/js1.js
+++ js/js1.js
@@ -1,6 +1,6 @@
 function OpenSideBar() {
     console.log("Hello!")
-    if (document.getElementById("sidebar").style.width = "0px") {
+    if (document.getElementById("sidebar").style.width == "0px") {
         document.getElementById("sidebar").style.width = "250px";
     }
     else {
