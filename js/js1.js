6e35f37889e7b15721dafb10132e0bdfb94c89ec
Caio Maia <caio.maiasb@gmail.com>
2021-05-18 14:07:26 -0300

    Adição de JavaScript para apresentação de sidebar; (Abre o sidebar, porém, não fecha)

---
+++ js/js1.js
@@ -0,0 +1,9 @@
+function OpenSideBar() {
+    console.log("Hello!")
+    if (document.getElementById("sidebar").style.width = "0px") {
+        document.getElementById("sidebar").style.width = "250px";
+    }
+    else {
+        document.getElementById("sidebar").style.width = "0px"
+    }
+}
