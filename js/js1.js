1e2692ac1ebc1b2a9fbf7477f741f61d2b1bbc53
Caio Maia <caio.maiasb@gmail.com>
2021-05-19 13:35:29 -0300

    ALTER COMMENT

--- js/js1.js
+++ js/js1.js
@@ -1,11 +1,11 @@
 function OpenSideBar() {
     if (document.getElementById("sidebar").style.width == "0px") {
         document.getElementById("sidebar").style.width = "250px";
-        /* document.getElementById("sidebar").style.height = "250px"; */
+        // document.getElementById("sidebar").style.height = "250px";
     }
     else {
         document.getElementById("sidebar").style.width = "0px"
-        /* document.getElementById("sidebar").style.height = "0px" */
+        // document.getElementById("sidebar").style.height = "0px" 
     }
 }
 
