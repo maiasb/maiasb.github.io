eb2608ce2c5849c1d61e21793a11a66b9cafbc35
Caio Maia <caio.maiasb@gmail.com>
2021-05-19 10:55:11 -0300

    Atualização de background html: retirada na barra cinza.; Início da criação de nova section e função para educação

--- js/js1.js
+++ js/js1.js
@@ -1,9 +1,14 @@
 function OpenSideBar() {
-    console.log("Hello!")
     if (document.getElementById("sidebar").style.width == "0px") {
         document.getElementById("sidebar").style.width = "250px";
+        /* document.getElementById("sidebar").style.height = "250px"; */
     }
     else {
         document.getElementById("sidebar").style.width = "0px"
+        /* document.getElementById("sidebar").style.height = "0px" */
     }
+}
+
+function OpenEduc() {
+    console.log("Hello!")
 }
