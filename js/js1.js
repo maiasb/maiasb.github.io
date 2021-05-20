1cc783154a2b1c7178ae5b67399c9fb336e3b472
Caio Maia <caio.maiasb@gmail.com>
2021-05-20 10:39:08 -0300

    Alteração na linguagem para pt; Alteração para sidebar abrir (width e height)

--- js/js1.js
+++ js/js1.js
@@ -1,11 +1,22 @@
-function OpenSideBar() {
+function SideBar() {
     if (document.getElementById("sidebar").style.width == "0px") {
-        document.getElementById("sidebar").style.width = "250px";
-        // document.getElementById("sidebar").style.height = "250px";
+        document.getElementById("sidebar").style.height = "250px"
+        document.getElementById("sidebar").style.width = "250px"
+
+        // setTimeout(() => {
+        //     document.getElementById("sidebar").style.transition = "0.5s"
+        //     document.getElementById("sidebar").style.width = "250px"
+        // }, 50);
+
     }
     else {
         document.getElementById("sidebar").style.width = "0px"
-        // document.getElementById("sidebar").style.height = "0px" 
+        document.getElementById("sidebar").style.height = "0px"
+
+        // setTimeout(() => {
+        //     document.getElementById("sidebar").style.transition = "0.5s"
+        //     document.getElementById("sidebar").style.height = "0px"
+        // }, 50);
     }
 }
 
